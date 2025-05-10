3491a52443c7bef9656fb132ddf6e0350aa6c619
Dmitry148889 <saturn148889@gmail.com>
2025-05-10 22:53:37 +0300

    Домашка4dsa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hellofe, fdgdfWorld!");
+    public static void main(String[] args)
+    {
+        System.out.println("Задача 1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.printf("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.printf("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.printf("значение отсутствует");
+        }
+        System.out.println("Задача 2");
+
     }
 }
